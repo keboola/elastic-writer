c1128f9173f8e2dfe8fae34fb4cebbc87ed965df
Martin Halamíček <martin@halamicek.cz>
2016-02-11 11:04:34 +0100

    more debug

--- phpunit.sh
+++ phpunit.sh
@@ -6,5 +6,10 @@ curl $EX_ES_HOST:$EX_ES_HOST_PORT/_cluster/health?pretty
 
 env
 
+cat /etc/hosts
+
+curl $ELASTICSEARCH13_PORT_9200_TCP_ADDR:$ELASTICSEARCH13_PORT_9200_TCP_PORT
+curl $ELASTICSEARCH22_PORT_9200_TCP_ADDR:$ELASTICSEARCH22_PORT_9200_TCP_PORT
+
 curl elasticsearch13:9200/_cluster/health?pretty
 curl elasticsearch22:9200/_cluster/health?pretty
